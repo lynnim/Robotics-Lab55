99d150f831245d56085603c4c291613eeca0e061
Lynn Im <yli2101@barnard.edu>
2018-12-11 14:56:30 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -102,7 +102,10 @@ class Follower:
             r_cy = int(R['m01'] / R['m00'])
             cv2.circle(image, (r_cx, r_cy), 10, (0, 0, 225), -1)
             print("red = stop")
-            self.twist.linear.x = 0.2
+            self.twist.linear.x = 0.3
+            self.twist.angular.z = 0
+            self.cmd_vel_pub.publish(self.twist)
+            self.twist.linear.x = 0
             self.twist.angular.z = 0
             self.cmd_vel_pub.publish(self.twist)
 
