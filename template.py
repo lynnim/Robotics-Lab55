1b4121cdb96435a5c3b4ae09675837f41cfba7f3
Lynn Im <yli2101@barnard.edu>
2018-12-12 11:34:55 -0500

    Update template.py

--- template.py
+++ template.py
@@ -3,7 +3,7 @@ import numpy as np
  
 img = cv2.imread("shape.png")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-template = cv2.imread("left.png", cv2.IMREAD_GRAYSCALE)
+template = cv2.imread("left.png")
 w, h = template.shape[::-1]
  
 result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
