d012d681bc06790a6ee6077efde1f540081996ab
Lynn Im <yli2101@barnard.edu>
2018-12-12 10:22:38 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,83 +1,21 @@
 #!/usr/bin/env python
-import rospy, cv2, cv_bridge, numpy
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
+import cv2 
+import numpy as np 
 
+# Read the main image 
+img = cv2.imread("shape.png") 
 
-"""
-Yellow
-[[[ 29 191 171]]]
-Blue
-[[[120 255 169]]]
-Red
-[[[  0 229 164]]]
-Green
-[[[ 60 193 169]]]
-Take [H-10, 100, 100] and [H+10, 255, 255] as lower and upper bounds, respectively 
-"""
+# Convert to grayscale 
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
-class Follower:
-  def __init__(self):
-    self.bridge = cv_bridge.CvBridge()
-    cv2.namedWindow("window", 1)
-    self.image_sub = rospy.Subscriber('camera/rgb/image_raw', Image, self.image_callback)
-    self.cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
-    self.twist = Twist()
+# Read the template 
+template1 = cv2.imread("left.png", cv2.IMREAD_GRAYSCALE) 
+#template2 = cv2.imread("right.png", cv2.IMREAD_GRAYSCALE)
 
-  def image_callback(self, msg):
-    image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+result1 = cv2.matchTemplate(gray_img, template1, cv2.TM_CCOEFF_NORMED)
+#result2 = cv2.matchTemplate(gray_img, template2, cv2.TM_CCOEFF_NORMED)
 
-    lower_yellow = numpy.array([19, 100, 100])
-    upper_yellow = numpy.array([39, 255, 255])
-    y_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-
-    lower_red = numpy.array([-10, 100, 100])
-    upper_red = numpy.array([10, 255, 255])
-    r_mask = cv2.inRange(hsv, lower_red, upper_red)
-
-    h, w, d = image.shape
-
-    #for the yellow
-    search_top = 5 * h / 6  # put the frame 5/6 of the way down
-    search_bot = search_top + 20  # use the 20 units in front of the robot from that 5/6 of the way down
-    y_mask[0:search_top, 0:w] = 0
-    y_mask[search_bot:h, 0:w] = 0
-
-    #for the red
-    r_top = 4 * h / 5  
-    r_bot = r_top + 20  # use the 20 units in front of the robot from that 5/6 of the way down
-    r_mask[0:r_top, 0:w] = 0
-    r_mask[r_bot:h, 0:w] = 0
-    R = cv2.moments(r_mask)
-
-    M = cv2.moments(y_mask)
-    if M['m00'] == 0:
-        if R['m00'] > 0:
-            # calculate the centriod
-            r_cx = int(R['m10'] / R['m00'])
-            r_cy = int(R['m01'] / R['m00'])
-            cv2.circle(image, (r_cx, r_cy), 10, (0, 0, 225), -1)
-            print("red = stop")
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = .05
-            self.cmd_vel_pub.publish(self.twist)
-
-        else:
-            # calculate the centriod
-            cx = int(M['m10'] / M['m00'])
-            cy = int(M['m01'] / M['m00'])
-            cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
-
-            err = cx - w / 2
-            self.twist.linear.x = 0.2
-            self.twist.angular.z = -float(err) / 100
-            #print("twisting:")
-            #print(str(-float(err) / 100))
-            self.cmd_vel_pub.publish(self.twist)
-    cv2.imshow("window", image)
-    cv2.waitKey(3)
-
-rospy.init_node('follower')
-follower = Follower()
-rospy.spin()
+cv2.imshow("img", img)
+cv2.imshow("result1", result1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
