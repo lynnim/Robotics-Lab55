e76e6580e758baf14f9fd0ff2894f439125fdfea
Lynn Im <yli2101@barnard.edu>
2018-12-11 19:58:24 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -9,7 +9,7 @@ gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 template = cv2.imread("star.png", cv2.IMREAD_GRAYSCALE) 
 
 result = cv2.matchTemplate(gray_img,template,cv2.TM_CCOEFF_NORMED) 
-
+print(result)
 
 cv2.imshow("img", img)
 cv2.imshow("result", result)
