ecedc3bfa473fee29306abff8ff519c875e10290
Lynn Im <yli2101@barnard.edu>
2018-12-11 19:50:24 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -1,31 +1,17 @@
-import rospy, cv2, cv_bridge, numpy
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
+import cv2
+import numpy as np
 
 # Read the main image 
-img_rgb = cv2.imread('shape.png')
+img = cv2.imread("shape.png")
 
 # Convert it to grayscale 
-img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+template = cv2.imread("star.png", cv2.IMREAD_GRAYSCALE) 
 
-# Read the template 
-template = cv2.imread('template',0) 
+result = cv2.matchTemplate(gray_img,template,cv2.TM_CCOEFF_NORMED) 
 
-# Store width and heigth of template in w and h 
-w, h = template.shape[::-1] 
 
-# Perform match operations. 
-res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED) 
-
-# Specify a threshold 
-threshold = 0.8
-
-# Store the coordinates of matched area in a numpy array 
-loc = np.where( res >= threshold) 
-
-# Draw a rectangle around the matched region. 
-for pt in zip(*loc[::-1]): 
-    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2) 
-
-# Show the final image with the matched area. 
-cv2.imshow('Detected',img_rgb) 
+cv2.imshow("img", img)
+cv2.imshow("result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
