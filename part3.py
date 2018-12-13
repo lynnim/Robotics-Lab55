41a9be470f4989d49c9823187ed118987304ed24
Lynn Im <yli2101@barnard.edu>
2018-12-13 04:38:46 -0500

    Update part3.py

--- part3.py
+++ part3.py
@@ -87,7 +87,7 @@ class Follower:
             print("right arrow: " + str(min_val2))
             print("star: " + str(min_val3))
 
-            if min_val1 > min_val2 and min_val1 > -0.16:
+            if min_val1 > min_val2 and min_val1 >= -0.16:
                 print("turning left")
                 self.twist.linear.x = .2
                 self.twist.angular.z = .05
