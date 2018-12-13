b97efeec7a02de93109525b250e18cb9d364933f
Lynn Im <yli2101@barnard.edu>
2018-12-13 00:51:52 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -129,7 +129,11 @@ class Follower:
                 self.twist.linear.x = 1
                 self.twist.angular.z = -1
                 self.cmd_vel_pub.publish(self.twist)
-            for s2 in range(10):
+            for s2 in range(20):
+                self.twist.linear.x = 10
+                self.twist.angular.z = 5
+                self.cmd_vel_pub.publish(self.twist)
+            for s3 in range(10):
                 self.twist.linear.x = 10
                 self.twist.angular.z = 5
                 self.cmd_vel_pub.publish(self.twist)
