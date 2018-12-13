067a28d3542ef0273838b8b4b678fbe8a8f6307a
Lynn Im <yli2101@barnard.edu>
2018-12-12 21:21:33 -0500

    Update contour.py

--- contour.py
+++ contour.py
@@ -12,7 +12,6 @@ mask = cv2.inRange(hsv, lower_red, upper_red)
 
 _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-
 cv2.drawContours(image, contours, -1, (0,255,0), 3)
 
 cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
