a5938a34a018312ae08f9a56639976cba59a2f3c
Lynn Im <yli2101@barnard.edu>
2018-12-12 19:50:50 -0500

    Update template1.py

--- template1.py
+++ template1.py
@@ -15,7 +15,7 @@ class Follower:
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
     img_rgb = cv2.imread(image) 
-	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
     #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     lower_yellow = numpy.array([19, 100, 100])
