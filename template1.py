2de730ed345ff81a48179f974ae35b9e4b998c57
Lynn Im <yli2101@barnard.edu>
2018-12-12 19:50:21 -0500

    Update template1.py

--- template1.py
+++ template1.py
@@ -14,7 +14,10 @@ class Follower:
 
   def image_callback(self, msg):
     image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    img_rgb = cv2.imread(image) 
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
+    #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
     lower_yellow = numpy.array([19, 100, 100])
     upper_yellow = numpy.array([39, 255, 255])
     mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
@@ -26,30 +29,16 @@ class Follower:
     mask[search_bot:h, 0:w] = 0
     M = cv2.moments(mask)
     if M['m00'] > 0:
-		#calculate the centriod
-		cx = int(M['m10']/M['m00'])
-		cy = int(M['m01']/M['m00'])
-		cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
-		# BEGIN CONTROL
-		err = cx - w/2
-		self.twist.linear.x = 0.2
-		self.twist.angular.z = -float(err) / 100
-		self.cmd_vel_pub.publish(self.twist)
-		# END CONTROL
-
-    else:
-	    	img_rgb = cv2.imread(image) 
-		img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
-		
-		# template matching
-		template = cv2.imread("leftred.png",0) 
-		w, h = template.shape[::-1]
-		 
-		result = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-		loc = numpy.where(result >= 0.4)
-		 
-		for pt in zip(*loc[::-1]):
-		    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+      #calculate the centriod
+      cx = int(M['m10']/M['m00'])
+      cy = int(M['m01']/M['m00'])
+      cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+      # BEGIN CONTROL
+      err = cx - w/2
+      self.twist.linear.x = 0.2
+      self.twist.angular.z = -float(err) / 100
+      self.cmd_vel_pub.publish(self.twist)
+      # END CONTROL
     cv2.imshow("window", img_rgb)
     cv2.waitKey(3)
 
@@ -57,39 +46,3 @@ rospy.init_node('follower')
 follower = Follower()
 rospy.spin()
 # END ALL
-
-
-
-
-
-
-
-
-#     image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-#     img_rgb = cv2.imread(image) 
-# 	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) 
-	
-# 	# template matching
-# 	template = cv2.imread("leftred.png",0) 
-# 	w, h = template.shape[::-1]
-	 
-# 	result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
-# 	loc = numpy.where(result >= 0.4)
-	 
-# 	for pt in zip(*loc[::-1]):
-# 	    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
- 
-#     cv2.imshow("window", image)
-#     cv2.waitKey(3)
-#     # cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-# 	# cv2.resizeWindow("img", 600, 600) 
-# 	# cv2.imshow("img", image)
-
-# rospy.init_node('follower')
-# follower = Follower()
-# rospy.spin()
-
- 
-
-
- 
