8bd849de61956bff1f586fca18cfbea87473102d
Lynn Im <yli2101@barnard.edu>
2018-12-12 21:16:42 -0500

    Create contour.py

---
+++ contour.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('leftarr.png')
+blurred = cv2.GaussianBlur(image, cv2.COLOR_BGR2HSV)
+
+lower_red = numpy.array([-10, 100, 100])
+upper_red = numpy.array([10, 255, 255])
+mask = cv2.inRange(hsv, lower_red, upper_red)
+
+
+_, contours, _ = cv2.findCountrous(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+
+cv2.drawContours(image, contours, -1, (0,255,0), 3)
+
+cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+cv2.imshow('Display', image)
+cv2.waitKey()
