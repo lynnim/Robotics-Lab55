45ddc2f16bf1fb48ec4806ea2fe664ea52daf926
Lynn Im <yli2101@barnard.edu>
2018-12-12 22:15:35 -0500

    Update contour.py

--- contour.py
+++ contour.py
@@ -5,14 +5,13 @@ image1 = cv2.imread('leftarr.png')
 blurred = cv2.GaussianBlur(image1, (5,5), 0)
 hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-image2 = cv2.imread('star.png')
-blurred2 = cv2.GaussianBlur(image2, (5,5), 0)
-hsv = cv2.cvtColor(blurred2, cv2.COLOR_BGR2HSV)
+# image2 = cv2.imread('star.png')
+# blurred2 = cv2.GaussianBlur(image2, (5,5), 0)
+# hsv = cv2.cvtColor(blurred2, cv2.COLOR_BGR2HSV)
 
 lower_red = np.array([-10, 100, 100])
 upper_red = np.array([10, 255, 255])
 mask = cv2.inRange(hsv, lower_red, upper_red)
-
 
 contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 print(contours)
@@ -21,7 +20,7 @@ print(contours)
 cv2.drawContours(image2, contours, -1, (255,0,0), 3)
 
 cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
-cv2.imshow('star', image1)
-#cv2.imshow('left_arrow', image2)
+cv2.imshow('left_arrow', image1)
+#cv2.imshow('star', image2)
 cv2.waitKey()
 
